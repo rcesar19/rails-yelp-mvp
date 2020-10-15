743bdd05abc9ffd977e68665364744a0ea9e0c0f
roberto oliveira <roberto.c.o@gmail.com>
2020-10-15 20:02:55 -0300

    yeld

---
+++ db/migrate/20201013233535_create_restaurants.rb
@@ -0,0 +1,14 @@
+class CreateRestaurants < ActiveRecord::Migration[6.0]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :address
+      t.integer :phone_number
+      t.string :category
+      t.integer :rating
+      t.string :content
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+pequim = { name: "Pequim", address: "7 Boundary St, London E2 7JE", phone_number: 12345678, category: "chinese", rating: 5 }
+baggio = { name: "Baggio", address: "56A Shoreditch High St, London E1 6PQ", phone_number: 0234567, category: "italian", rating: 4 }
+honda = { name: "Honda", address: "9 Boundary St, London E2 7JE", phone_number: 12345678, category: "japanese", rating: 2 }
+zidane = { name: "Zidane", address: "10 Boundary St, London E2 7JE", phone_number: 43345678, category: "french", rating: 4 }
+lukaku = { name: "Lukaku", address: "11 Boundary St, London E2 7JE", phone_number: 14345678, category: "belgian", rating: 3 }
+
+[ pequim, baggio, honda, zidane, lukaku ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
